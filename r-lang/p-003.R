7ab17b4dde1f64a4fe3866aa92b0c432f333a9f4
Paige <pnkrsnak@gmail.com>
2016-02-04 17:21:18 -0500

    one of those days...

--- r-lang/p-003.R
+++ r-lang/p-003.R
@@ -3,21 +3,30 @@
 
 # helper functions
 is.prime <- function(x) {
+	prm <- TRUE
 	# quickly get rid of multiples of 2,3
-	if (x %% 2 == 0 && x != 2) { FALSE }
-	else if (x %% 3 == 0 && x != 3) { FALSE }
+	if (x %% 2 == 0 || x == 2) { 
+		print('gotta2')		
+		print(prm)
+		return(!prm) }
+	else if (x %% 3 == 0 || x == 3) { !prm }
 	else { 
 		testceil <- ceiling(sqrt(x))	 
+		print(testceil)
 		for (n in 5:testceil) {
-			if (x %% n == 0) { FALSE }		
+			if (x %% n == 0) { 
+				break }
 		}
-		TRUE
+	print('yeep')	
+	return(prm)
 	}
-}
+	return(prm)
+} 
 
 # this returns an array, in desc order, of prime factors
 rtrn.prme.arry <- function(x) {
 	if (is.prime(x)) {
+		print('im prime')
 		x
 	} else {
 		myarray <- c()
@@ -26,16 +35,15 @@ rtrn.prme.arry <- function(x) {
 			testceil <- ceiling(sqrt(myd))
 			for (n in 2:testceil) {
 				if (is.prime(n) && (myd %% n == 0)) {
-					# fix this, it needs to check for powers of its divisor
-					myarray <- c(n, myarray)
-					myd <- (myd/n)
-					next } } }
-	myarray <- c(myd, myarray)
+					while (myd %% n == 0) {
+						myd <- myd/n
+						myarray <- c(n, myarray) 
+					} } } }
 	sort(myarray, decreasing = TRUE) }
 }
 
-
-
 # MAIN
 
-print(rtrn.prme.arry(7000))
+#print(rtrn.prme.arry(7000))
+#[1] 7 5 5 5 2 2 2
+print(rtrn.prme.arry(38))
