91d4f6cc1121ba6e64cf6bc4e9d1aa77822dd434
Paige <pnkrsnak@gmail.com>
2016-03-04 15:26:04 -0500

    scribbles

--- r-lang/p-004.R
+++ r-lang/p-004.R
@@ -5,4 +5,34 @@
 a <- 91
 b <- 99
 c <- a*b
-print(c)
+d <- 100001
+e <- 100002
+
+
+# helper fcns
+gimmearray <- function(num) {
+	digits <- nchar(num, type = "chars")
+	arr <- 0
+	for (i in 1:digits) {
+		res <- num %% 10
+		arr[i] <- res
+		num <- floor(num/10)
+	}
+	return(arr)
+}
+
+verif_palin <- function(array) {
+	if (identical(array,rev(array))) {
+		return('TRUE')
+	} else {
+		return('FALSE')
+	}
+}
+
+
+# MAIN
+arr1 <- gimmearray(d)
+arr2 <- gimmearray(e)
+if (verif_palin(arr1)) {
+	print(arr1)
+}
