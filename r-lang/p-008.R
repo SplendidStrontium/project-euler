30970636e2578a83b142335c0695287e23d7ab05
PaigeNK <pnkrsnak@gmail.com>
2017-05-22 18:46:37 -0400

    p8 complete
    note the number is not a matrix, so trying to find horiz and vert
    matches is not correct. whoops.

--- r-lang/p-008.R
+++ r-lang/p-008.R
@@ -27,35 +27,21 @@ mylist <- '73167176531330624919225119674426574742355349194934
 mylist <- gsub("[\r\n]", "", mylist)
 
 commalist <- strsplit(mylist, character(0))
-vec <- numeric(0)
+srcno <- numeric(0)
 for (e in commalist) {
-	vec <- c(vec, e)
+	srcno <- c(srcno, as.numeric(e))
 }
-	
-# MAIN
 
-A <- matrix(
-	vec,
-	nrow=50,
-	ncol=20
-)
-A <- t(A)
-
-# A is imported
-# syntax is A[row,col]
 
 bigp <- 1
 bigvec <- numeric(0)
 
-for (row in 1:20) {
-	myrow <- as.numeric(A[row, ])
-
-	for (n in 1:38) {
-		vec <- numeric(0)
-		for (i in 1:13) {
-			vec[i] <- myrow[i+n-1]
-		}
-		myprod <- prod(vec[vec != 0])
+for (i in 1:988) {
+	vec <- numeric(0)
+	for (n in 1:13) {
+		vec[n] <- srcno[i+n-1]
+		myprod <- prod(vec)
+		# myprod <- prod(vec[vec != 0])
 		if (myprod > bigp) {
 			bigp <- myprod
 			bigvec <- vec }
@@ -64,18 +50,3 @@ for (row in 1:20) {
 print(bigvec)
 print(bigp)
 
-for (col in 1:50) {
-	for (row in 1:8) {
-		vec <- numeric(0)
-		for (n in 1:13) {
-			vec[n] <- as.numeric(A[row+n-1,col])
-		}
-		myprod <- prod(vec[vec != 0])
-		if (myprod > bigp) {
-			bigp <- myprod
-			bigvec <- vec }
-	}
-}
-
-print(bigvec)
-print(bigp)
