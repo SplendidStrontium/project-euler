0b16812ccedbe3a4efb9f8e48c3cbd25d3d0d1fe
PaigeNK <pnkrsnak@gmail.com>
2018-02-11 14:54:16 -0500

    pascals triangle

--- r-lang/p-015.r
+++ r-lang/p-015.r
@@ -4,6 +4,7 @@
 #binomial coeff
 print(choose(40,20))
 
+#construct pascals triangle
 mym <- matrix(, nrow = 21, ncol = 21)
 
 for (row in 1:21){
@@ -12,7 +13,11 @@ for (row in 1:21){
 for (col in 1:21){
 	mym[1,col] <- 1
 }
+for (row in 2:21){
+	for (col in 2:21){
+		mym[row,col] <- mym[row-1,col] + mym[row,col-1]
+	}
+}
 
-#one may construct a pascals triangle from here but Im concerned about length of the answer
-
-print(mym)
+print(mym[21,21])
+#trees and recursion take too much time?
