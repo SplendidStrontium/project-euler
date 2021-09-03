e37075757162a716cc608f92bdab1c77cdf8f2ea
Paige <nan@nan.nyc>
2021-09-03 13:52:07 -0400

    solved p022

--- r-lang/p-022.r
+++ r-lang/p-022.r
@@ -7,45 +7,46 @@
 
 #BEGIN function to return letter value
 lettervalue <- function(char) {
-	if (char == "a") {return(1)}
-	if (char == "b") {return(2)}
-	if (char == "c") {return(3)}
-	if (char == "d") {return(4)}
-	if (char == "e") {return(5)}
-	if (char == "f") {return(6)}
-	if (char == "g") {return(7)}
-	if (char == "h") {return(8)}
-	if (char == "i") {return(9)}
-	if (char == "j") {return(10)}
-	if (char == "k") {return(11)}
-	if (char == "l") {return(12)}
-	if (char == "m") {return(13)}
-	if (char == "n") {return(14)}
-	if (char == "o") {return(15)}
-	if (char == "p") {return(16)}
-	if (char == "q") {return(17)}
-	if (char == "r") {return(18)}
-	if (char == "s") {return(19)}
-	if (char == "t") {return(20)}
-	if (char == "u") {return(21)}
-	if (char == "v") {return(22)}
-	if (char == "w") {return(23)}
-	if (char == "x") {return(24)}
-	if (char == "y") {return(25)}
-	if (char == "z") {return(26)}
+	if (char == "A") {return(1)}
+	if (char == "B") {return(2)}
+	if (char == "C") {return(3)}
+	if (char == "D") {return(4)}
+	if (char == "E") {return(5)}
+	if (char == "F") {return(6)}
+	if (char == "G") {return(7)}
+	if (char == "H") {return(8)}
+	if (char == "I") {return(9)}
+	if (char == "J") {return(10)}
+	if (char == "K") {return(11)}
+	if (char == "L") {return(12)}
+	if (char == "M") {return(13)}
+	if (char == "N") {return(14)}
+	if (char == "O") {return(15)}
+	if (char == "P") {return(16)}
+	if (char == "Q") {return(17)}
+	if (char == "R") {return(18)}
+	if (char == "S") {return(19)}
+	if (char == "T") {return(20)}
+	if (char == "U") {return(21)}
+	if (char == "V") {return(22)}
+	if (char == "W") {return(23)}
+	if (char == "X") {return(24)}
+	if (char == "Y") {return(25)}
+	if (char == "Z") {return(26)}
 }
 #END function to return letter value
 
 
-#BEGIN function to return vector from input name
-characterize <- function(x) {
-	namearray <- NULL
-	strsplit(x)
-	for (place in 1:length(x)) {
-		namearray[place]
+#BEGIN function to return name value
+namevalue <- function(name) {
+	name.char <- unlist(strsplit(name, split = ""))
+	mysum <- 0
+	for (place in 1:length(name.char)) {
+		mysum <- mysum + lettervalue(name.char[place])
 	}
+	return(mysum)
 }
-#END function to return vector from input name
+#END function to return name value
 
 
 #BEGIN main logic
@@ -53,14 +54,15 @@ characterize <- function(x) {
 #first, proper file i/o must be addressed
 #namelist <- read.delim("p022_names.txt", header = FALSE, sep = ",", dec = ".")
 namelist <- scan("p022_names.txt", what= "", sep=",", na.strings="")
-print(namelist)
+namelist <- sort(namelist)
 
-#split the string into single characters
+totalsum <- 0
+for (i in 1:length(namelist)){
+	totalsum <- totalsum + (i * namevalue(namelist[i]))
+}
 
-#grab the last element and delete it from the array
-namestr <- namelist[length(namelist)]
-namelist[length(namelist)] <- NULL
-
-print(nchar(namestr))
-namestr <- strsplit(namestr, split = "")
+print(totalsum)
 #END main logic
+
+#solved sep 3 2021
+#sum of namevalues is 871198282
